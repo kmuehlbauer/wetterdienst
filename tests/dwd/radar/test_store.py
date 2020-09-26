a2055c79f05a789c89d5e9ad8c0a21e0a3c28b9c
Andreas Motl <andreas.motl@panodata.org>
2020-09-26 17:30:04 +0200

    Improve radar data acquisition
    - Naming things
    - Improve indexing for directories containing both BIN and BUFR
    - More tests for "test_radar_fileindex"

--- tests/dwd/radar/test_store.py
+++ tests/dwd/radar/test_store.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-from wetterdienst import Parameter, TimeResolution
+from wetterdienst import TimeResolution
 from wetterdienst.dwd.metadata.constants import DWD_FOLDER_MAIN
+from wetterdienst.dwd.radar.metadata import RadarParameter
 from wetterdienst.dwd.radar.store import _build_local_filepath
 
 
 def test_build_local_filepath_for_radar():
 
     radar_filepath = _build_local_filepath(
-        Parameter.DX_REFLECTIVITY,
+        RadarParameter.DX_REFLECTIVITY,
         datetime(2020, 1, 1, 12, 15),
         DWD_FOLDER_MAIN,
         TimeResolution.MINUTE_5,
